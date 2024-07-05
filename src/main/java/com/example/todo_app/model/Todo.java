819c75110c8cca15241fea0121a61aef39568585
RaulErnesto08 <ernesto.villarreal@encora.com>
2024-07-05 14:24:24 -0600

    Add validation to model

--- src/main/java/com/example/todo_app/model/Todo.java
+++ src/main/java/com/example/todo_app/model/Todo.java
@@ -1,16 +1,29 @@
 package com.example.todo_app.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
 public class Todo {
     private Long id;
+
+    @NotBlank
+    @Size(max = 120)
     private String text;
-    private LocalDateTime dueDate;
+
+    private LocalDate dueDate;
     private boolean done;
     private LocalDateTime doneDate;
+
+    @NotNull
     private Priority priority;
+
     private LocalDateTime creationDate;
 
     public enum Priority {
